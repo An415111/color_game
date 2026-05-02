98bdbedcb84ca0ba5a1789d991cd60e73903f2b4
music_project <ankitgupta37937@gmail.com>
2026-05-02 11:31:19 +0530

    Renamed style to public

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-app.use(express.static("style"));
+app.use(express.static("public"));
 
 app.get("/" , function(req,res){
 	res.render("index.ejs");
